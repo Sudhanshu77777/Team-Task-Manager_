5b7d9bf732c59a22417cd78bf7a4bd7290f524a1
Sudhanshu <sudhanshuthakur042004@gmail.com>
2026-05-20 17:43:31 +0530

    initial commit

---
+++ backend/controllers/taskController.js
@@ -0,0 +1,52 @@
+const Task = require("../models/Task");
+
+exports.createTask = async (req, res) => {
+
+    try {
+
+        const task = await Task.create(req.body);
+
+        res.status(201).json(task);
+
+    } catch (error) {
+
+        res.status(500).json(error);
+
+    }
+};
+
+exports.getTasks = async (req, res) => {
+
+    try {
+
+        const tasks = await Task.find()
+        .populate("assignedTo")
+        .populate("project");
+
+        res.json(tasks);
+
+    } catch (error) {
+
+        res.status(500).json(error);
+
+    }
+};
+
+exports.updateTaskStatus = async (req, res) => {
+
+    try {
+
+        const task = await Task.findByIdAndUpdate(
+            req.params.id,
+            { status: req.body.status },
+            { new: true }
+        );
+
+        res.json(task);
+
+    } catch (error) {
+
+        res.status(500).json(error);
+
+    }
+};
